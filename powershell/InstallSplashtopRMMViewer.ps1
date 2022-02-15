20805672917238ae0eda8484977f0530b4cc25a9
Aaron Stevenson <aaron@redletter.tech>
2022-02-15 15:55:35 -0600

    Update InstallSplashtopRMMViewer.ps1

--- powershell/InstallSplashtopRMMViewer.ps1
+++ powershell/InstallSplashtopRMMViewer.ps1
@@ -1,17 +1,21 @@
 # Installs Splashtop for RMM Viewer
 $Installer = "$env:Temp\SplashtopViewer.exe"
 $DownloadURL = '' # Add download URL from your RMM provider here
+$FileHandlerPrefix = 'st-rmm://*'
+$RegValue = '200' 
 
 try {
   # Download Splashtop for RMM Viewer
+  Write-Output 'Downloading Splashtop for RMM viewer...'
   Invoke-WebRequest -Uri $DownloadURL -OutFile $Installer
 
   # Install Splashtop for RMM Viewer
+  Write-Output 'Installing Splashtop for RMM viewer...'
   Start-Process -Wait -FilePath $Installer -ArgumentList 'prevercheck /s /i'
 
   # Trust Splashtop RMM URL File Handler in Chrome & Edge (Chromium)
-  Reg.exe add 'HKLM\SOFTWARE\Policies\Google\Chrome\URLWhitelist' /v '200' /t REG_SZ /d 'st-rmm://*' /f
-  Reg.exe add 'HKLM\SOFTWARE\Policies\Microsoft\Edge\URLAllowlist' /v '200' /t REG_SZ /d 'st-rmm://*' /f
+  Reg.exe add 'HKLM\SOFTWARE\Policies\Google\Chrome\URLWhitelist' /v $RegValue /t REG_SZ /d $FileHandlerPrefix /f | Out-Null
+  Reg.exe add 'HKLM\SOFTWARE\Policies\Microsoft\Edge\URLAllowlist' /v $RegValue /t REG_SZ /d $FileHandlerPrefix /f | Out-Null
 }
 catch { throw $Error }
 finally {
