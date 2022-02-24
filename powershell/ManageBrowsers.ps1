fc7cd1fdd3d5ee45e5e0cb0d61a6dfeda836284e
Aaron Stevenson <aaron@redletter.tech>
2022-02-24 15:37:09 -0600

    Create ManageBrowsers.ps1
    Discussion #5

---
+++ powershell/ManageBrowsers.ps1
@@ -0,0 +1,158 @@
+<#
+  .SYNOPSIS
+    Manages browser settings through local group policy objects (LGPO).
+  .PARAMETER Audit
+    Outputs a table of the current browser management policies (LGPO). No new policies will be set.
+  .PARAMETER Reset
+    Resets all existing browser management policies (LGPO). No new policies will be set.
+  .PARAMETER SearchEngine
+    Specify the default search engine. Supported values are 'Google' or 'Bing'.
+#>
+
+param (
+  [Alias('Report')][switch]$Audit,        # Audit extension management policies (LGPO)
+  [Alias('Clear')][switch]$Reset,         # Reset browser management policies
+  [Alias('Search')][string]$SearchEngine  # Default search engine
+)
+
+$ComputerPolicyFile = ($env:SystemRoot + '\System32\GroupPolicy\Machine\registry.pol')
+$ChromeKey = 'Software\Policies\Google\Chrome'
+$ChromeUpdateKey = 'Software\Policies\Google\Update'
+$EdgeKey = 'Software\Policies\Microsoft\Edge'
+$EdgeUpdateKey = 'Software\Policies\Microsoft\EdgeUpdate'
+
+# Default Search Engine for browser policies
+if ($SearchEngine) {
+  switch ($SearchEngine) {
+    'Google' {
+      $SearchURL = '{google:baseURL}search?q={searchTerms}&{google:RLZ}{google:originalQueryForSuggestion}{google:assistedQueryStats}{google:searchFieldtrialParameter}{google:searchClient}{google:sourceId}ie={inputEncoding}'
+      $SuggestURL = '{google:baseURL}complete/search?output=chrome&q={searchTerms}'
+    }
+    'Bing' {
+      $SearchURL = '{bing:baseURL}search?q={searchTerms}'
+      $SuggestURL = '{bing:baseURL}qbox?query={searchTerms}'
+    }
+    default { throw "$SearchEngine is not supported." }
+  }
+}
+
+Write-Output "`nChecking for necessary PowerShell modules..."
+try {
+  # Set PowerShell to TLS 1.2 (https://devblogs.microsoft.com/powershell/powershell-gallery-tls-support/)
+  [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+  # Install NuGet package provider
+  if (!(Get-PackageProvider -ListAvailable -Name 'NuGet' -ErrorAction Ignore)) {
+    Write-Output 'Installing NuGet package provider...'
+    Install-PackageProvider -Name 'NuGet' -MinimumVersion 2.8.5.201 -Force
+  }
+
+  # Set PSGallery to trusted repository
+  Register-PSRepository -Default -InstallationPolicy 'Trusted' -ErrorAction Ignore
+  if (!(Get-PSRepository -Name 'PSGallery' -ErrorAction Ignore).InstallationPolicy -eq 'Trusted') {
+    Set-PSRepository -Name 'PSGallery' -InstallationPolicy 'Trusted'
+  }
+  
+  # Install & Import PolicyFileEditor module
+  if (!(Get-Module -ListAvailable -Name 'PolicyFileEditor' -ErrorAction Ignore)) {
+    Write-Output 'Installing PolicyFileEditor module...'
+    Install-Module -Name 'PolicyFileEditor'
+    Import-Module 'PolicyFileEditor'
+  }
+
+  Write-Output 'Modules installed.'
+}
+catch { 
+  throw $Error
+  exit
+}
+
+if ($Audit -or $Reset) {
+  $Policies = @()
+  $Keys = @($ChromeKey, $ChromeUpdateKey, $EdgeKey, $EdgeUpdateKey)
+  foreach ($Key in $Keys) { $Policies += Get-PolicyFileEntry -Path $ComputerPolicyFile -All | Where-Object { $_.Key -eq $Key } }
+  
+  if ($Audit) {
+    Write-Output "`nBrowser Management Policies (LGPO)"
+    $GroupBy = @{Label = 'Browser'; Expression = { 
+        switch -Wildcard ($_.Key) {
+          '*Google*' { 'Google Chrome' }
+          '*Microsoft*' { 'Microsoft Edge' }
+        }
+      }
+    }
+  
+    $TableProperties = @(
+      @{Label = 'Policy'; Expression = { $_.ValueName } },
+      @{Label = 'Value'; Expression = { $_.Data } }
+    )
+
+    $Policies | Format-Table -Property $TableProperties -GroupBy $GroupBy
+  }
+  elseif ($Reset) {
+    Write-Output "`nResetting browser management policies..."
+    $Policies | Remove-PolicyFileEntry -Path $ComputerPolicyFile
+    Write-Output "Browser management policies reset.`n"
+  }
+}
+else {
+  $Policies = @(
+    # Chrome Policies
+    [PSCustomObject]@{ Key = $ChromeKey; ValueName = 'AutoplayAllowed'; Data = '0'; Type = 'Dword' }                  # Disable media autoplay
+    [PSCustomObject]@{ Key = $ChromeKey; ValueName = 'BackgroundModeEnabled'; Data = '0'; Type = 'Dword' }            # Disable background mode
+    [PSCustomObject]@{ Key = $ChromeKey; ValueName = 'ConfigureDoNotTrack'; Data = '1'; Type = 'Dword' }              # Enable Do Not Track
+    [PSCustomObject]@{ Key = $ChromeKey; ValueName = 'ForceGoogleSafeSearch'; Data = '1'; Type = 'Dword' }            # Enable Google SafeSearch
+    [PSCustomObject]@{ Key = $ChromeKey; ValueName = 'DefaultNotificationsSetting'; Data = '2'; Type = 'Dword' }      # Disable desktop notifications
+    [PSCustomObject]@{ Key = $ChromeKey; ValueName = 'ProxyMode'; Data = 'direct'; Type = 'String' }                  # Disable proxy servers
+    [PSCustomObject]@{ Key = $ChromeKey; ValueName = 'SafeBrowsingEnabled'; Data = '1'; Type = 'Dword' }              # Enable SafeBrowsing
+    [PSCustomObject]@{ Key = $ChromeKey; ValueName = 'DisableSafeBrowsingProceedAnyway'; Data = '1'; Type = 'Dword' } # Prevent SafeBrowsing bypass
+    [PSCustomObject]@{ Key = $ChromeKey; ValueName = 'SafeSitesFilterBehavior'; Data = '1'; Type = 'Dword' }          # Filter adult content
+  
+    # Google Software Updates
+    [PSCustomObject]@{ Key = $ChromeUpdateKey; ValueName = 'InstallDefault'; Data = '4'; Type = 'Dword' }             # Allow machine-wide installs only
+    [PSCustomObject]@{ Key = $ChromeUpdateKey; ValueName = 'UpdateDefault'; Data = '1'; Type = 'Dword' }              # Always allow updates (all channels)
+  
+    # Microsoft Edge Policies
+    [PSCustomObject]@{ Key = $EdgeKey; ValueName = 'AutoplayAllowed'; Data = '0'; Type = 'Dword' }                    # Disable media autoplay
+    [PSCustomObject]@{ Key = $EdgeKey; ValueName = 'BackgroundModeEnabled'; Data = '0'; type = 'Dword' }              # Disable background mode
+    [PSCustomObject]@{ Key = $EdgeKey; ValueName = 'ConfigureDoNotTrack'; Data = '1'; Type = 'Dword' }                # Enable Do Not Track
+    [PSCustomObject]@{ Key = $EdgeKey; ValueName = 'DefaultNotificationsSetting'; Data = '2'; Type = 'Dword' }        # Disable desktop notifications
+    [PSCustomObject]@{ Key = $EdgeKey; ValueName = 'ForceBingSafeSearch'; Data = '1'; Type = 'Dword' }                # Enable Bing Safe Search (Moderate)
+    [PSCustomObject]@{ Key = $EdgeKey; ValueName = 'ForceGoogleSafeSearch'; Data = '1'; Type = 'Dword' }              # Enable Google SafeSearch
+    [PSCustomObject]@{ Key = $EdgeKey; ValueName = 'PreventSmartScreenPromptOverride'; Data = '1'; Type = 'Dword' }   # Prevent SmartScreen bypass
+    [PSCustomObject]@{ Key = $EdgeKey; ValueName = 'SmartScreenEnabled'; Data = '1'; Type = 'Dword' }                 # Enable SmartScreen
+    [PSCustomObject]@{ Key = $EdgeKey; ValueName = 'SmartScreenPuaEnabled'; Data = '1'; Type = 'Dword' }              # Block PUAs/PUPs
+    [PSCustomObject]@{ Key = $EdgeUpdateKey; ValueName = 'UpdateDefault'; Data = '1'; Type = 'Dword' }                # Always allow updates (all channels)
+    [PSCustomObject]@{                                                                                                # Disable Proxy Servers
+      Key = $EdgeKey;
+      ValueName = 'ProxySettings';
+      Data = '{"ProxyMode": "direct", "ProxyPacUrl": "", "ProxyServer": "", "ProxyBypassList": ""}';
+      Type = 'String' 
+    } 
+  )
+  
+  # Search Engine Policies
+  if ($SearchEngine) {
+    $Policies += @(
+      # Chrome
+      [PSCustomObject]@{ Key = $ChromeKey; ValueName = 'DefaultSearchProviderEnabled'; Data = '1'; Type = 'Dword' }             # Enable default search provider
+      [PSCustomObject]@{ Key = $ChromeKey; ValueName = 'DefaultSearchProviderName'; Data = $SearchEngine; Type = 'String' }     # Set default search provider name
+      [PSCustomObject]@{ Key = $ChromeKey; ValueName = 'DefaultSearchProviderSearchURL'; Data = $SearchURL; Type = 'String' }   # Set default search provider
+      [PSCustomObject]@{ Key = $ChromeKey; ValueName = 'DefaultSearchProviderSuggestURL'; Data = $SuggestURL; Type = 'String' } # Set default suggestion provider
+  
+      # Edge
+      [PSCustomObject]@{ Key = $EdgeKey; ValueName = 'DefaultSearchProviderEnabled'; Data = '1'; Type = 'Dword' }               # Enable default search provider
+      [PSCustomObject]@{ Key = $EdgeKey; ValueName = 'DefaultSearchProviderName'; Data = $SearchEngine; Type = 'String' }       # Set default search provider name
+      [PSCustomObject]@{ Key = $EdgeKey; ValueName = 'DefaultSearchProviderSearchURL'; Data = $SearchURL; Type = 'String' }     # Set default search provider
+      [PSCustomObject]@{ Key = $EdgeKey; ValueName = 'DefaultSearchProviderSuggestURL'; Data = $SuggestURL; Type = 'String' }   # Set default suggestion provider
+    )
+  }
+  
+  try {
+    Write-Output "`nSetting browser management policies..."
+    $Policies | Set-PolicyFileEntry -Path $ComputerPolicyFile
+    Write-Output "Browser management policies set.`n"
+    gpupdate /force /wait:0
+  }
+  catch { throw $Error }
+}
