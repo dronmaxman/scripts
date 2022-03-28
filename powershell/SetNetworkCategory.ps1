fed75dcab145e8deed1c621da27a1a9ef91fbd5b
Aaron Stevenson <aaron@redletter.tech>
2022-03-28 11:25:36 -0500

    Create SetNetworkCategory.ps1

---
+++ powershell/SetNetworkCategory.ps1
@@ -0,0 +1,28 @@
+# Sets current network category (type) to Public or Private.
+# Should be run with Administrator privileges.
+
+param (
+  [Parameter (Mandatory = $true)]
+  [ValidateSet('Public', 'Private', IgnoreCase)]
+  [string]$Category # Desired Network Category
+)
+
+# Format parameter
+$Category = (Get-Culture).TextInfo.ToTitleCase($Category)
+
+# Get Current Network Profile
+$CurrentNetwork = Get-NetConnectionProfile
+
+# Set Network Category
+try {
+  if ($CurrentNetwork.NetworkCategory -ne $Category) {
+    Set-NetConnectionProfile -Name $CurrentNetwork.Name -NetworkCategory $Category
+    Write-Output "Network category set to $Category."
+  }
+  else {
+    Write-Output "Network category was already set to $Category."
+  }
+}
+catch {
+  throw
+}
