8a7ec8405873e56c4a3481f6c72916567b9909d8
Aaron Stevenson <aaron@redletter.tech>
2022-04-03 22:40:26 -0500

    Create RemoveZeroTier.ps1

---
+++ powershell/RemoveZeroTier.ps1
@@ -0,0 +1,20 @@
+# Removes ZeroTier One
+$Paths = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall', 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall'
+$ZeroTierOne = Get-ChildItem -Path $Paths | Get-ItemProperty | Where-Object { $_.DisplayName -like 'ZeroTier One' } | Select-Object
+$VirtualNetworkPort = Get-ChildItem -Path $Paths | Get-ItemProperty | Where-Object { $_.DisplayName -like 'ZeroTier One Virtual Network Port' } | Select-Object
+
+if ($ZeroTierOne) {
+  Write-Output 'Uninstalling ZeroTier One...'
+  foreach ($Ver in $ZeroTierOne) {
+    $Uninst = $Ver.UninstallString
+    cmd /c $Uninst /qn
+  }
+}
+
+if ($VirtualNetworkPort) {
+  Write-Output 'Uninstalling ZeroTier Virtual Network Port...'
+  foreach ($Ver in $VirtualNetworkPort) {
+    $Uninst = $Ver.UninstallString
+    cmd /c $Uninst /qn
+  }
+}
