6f7f10bba58a90cd2ec69e13abae1a6a5b86ddf3
Aaron Stevenson <aaron@redletter.tech>
2022-05-06 11:02:01 -0500

    Create ScanDefender.ps1

---
+++ powershell/ScanDefender.ps1
@@ -0,0 +1,20 @@
+# Runs a Windows Defender Scan
+param(
+  [ValidateSet('Full', 'Quick', 'Offline', IgnoreCase)]  
+  [string]$Type = 'Quick', # Scan type (defaults to quick scan)
+  [System.IO.FileInfo]$Path # Optional local path to scan
+)
+
+# Format type parameter
+$Type = (Get-Culture).TextInfo.ToTitleCase($Type)
+
+# Update definitions
+Update-MpSignature
+
+# Run scan
+if ($Path) { Start-MpScan -ScanType 'Custom' -ScanPath $Path }
+elseif ($Type = 'Offline') { Start-MpWDOScan }
+else { Start-MpScan -ScanType $Type }
+
+# Remove active threats
+Remove-MpThreat
