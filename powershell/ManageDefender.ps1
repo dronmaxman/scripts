97906834c37ed85dfff7486132edbc8372698f47
Aaron Stevenson <aaron@redletter.tech>
2022-05-08 08:55:39 -0500

    Update ManageDefender.ps1

--- powershell/ManageDefender.ps1
+++ powershell/ManageDefender.ps1
@@ -91,7 +91,7 @@ $Policies = @(
   [PSCustomObject]@{Key = "$ExploitGuardKey\Network Protection"; ValueName = 'EnableNetworkProtection'; Data = '1'; Type = 'Dword' } # Block dangerous websites
 
   # Windows Defender ASR
-  # 0 (Off), 1 (Block), or 2 (Audit)
+  # ASR Rules - 0 (Off), 1 (Block), or 2 (Audit)
   [PSCustomObject]@{Key = "$ExploitGuardKey\ASR"; ValueName = 'ExploitGuard_ASR_Rules'; Data = '0'; Type = 'Dword' } # Enable ASR
   [PSCustomObject]@{Key = "$ExploitGuardKey\ASR\Rules"; ValueName = '7674ba52-37eb-4a4f-a9a1-f0f9a1619a2c'; Data = '0'; Type = 'String' } # Block Adobe Reader from creating child processes
   [PSCustomObject]@{Key = "$ExploitGuardKey\ASR\Rules"; ValueName = 'D4F940AB-401B-4EFC-AADC-AD5F3C50688A'; Data = '0'; Type = 'String' } # Block all Office applications from creating child processes
